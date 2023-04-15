2c51f0eae788ce5b5942ed0a71db109ed3f20d69
Mohit Alam Bhuiyan <mohitcseedu@gmail.com>
2023-04-16 03:57:20 +0600

    ready to cart

--- src/components/Watch/Watch.js
+++ src/components/Watch/Watch.js
@@ -5,6 +5,15 @@ const Watch = (props) => {
     return (
         <div className='watch'>
             <img src= {img} alt="" />
+
+            <div className='watch-info'>
+            <p className='watch-name'>{name}</p>
+            <p className='watch-price'>{price}</p>
+            </div>
+
+            <button className='btn-cart'>
+                <p>Add To Cart</p>
+            </button>
         </div>
     );
 };
